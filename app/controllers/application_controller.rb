123e5a7c222fc3591f7bd29e040e712aba8e11fa
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-01-14 08:20:50 +0100

    custom styles and fr translations added + default user_language is fr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_user_language
+  
+  private
+    def set_user_language
+      I18n.locale = 'fr'
+    end
 end
