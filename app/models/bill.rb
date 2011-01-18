eb60c0081fa0c389bbc2f3d4b6f399c94b9ca07d
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-01-18 08:14:08 +0100

    Bill.edit done

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,5 @@
 class Bill < ActiveRecord::Base
+  belongs_to :customer
+  delegate :firstname, :to => :customer, :prefix => true, :allow_nil => true
+  delegate :lastname, :to => :customer, :prefix => true, :allow_nil => true
 end
