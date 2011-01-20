329afbf8bed7ce675a7dae777d5e4448d366a050
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-01-20 07:36:52 +0100

    root_url configured + devise initializer and locales added

--- config/routes.rb
+++ config/routes.rb
@@ -66,7 +66,7 @@ Billit::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "customers#index"
 
   # See how all your routes lay out with "rake routes"
 
