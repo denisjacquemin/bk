c9abdfb8882ca73184a49d9c584b7ac6a41cdea3
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-01-24 07:56:07 +0100

    adding and removing products to a bill done

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,5 @@ class Product < ActiveRecord::Base
   belongs_to :bill
   belongs_to :tva
   
-  
   delegate :value, :to => :tva, :prefix => true, :allow_nil => true
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,11 @@
-var quicksearch = null;
 
+
+replace_ids = function(s){
+  var new_id = new Date().getTime();
+  return s.replace(/NEW_RECORD/g, new_id);
+}
+
+var quicksearch = null;
 document.observe("dom:loaded", function() {
   
   // init Quick Search on customers/index
@@ -11,4 +17,20 @@ document.observe("dom:loaded", function() {
   // put the focus on input with a .focus selector
   $$('.focus').invoke('select');
   
+  $$('.remove').each(function(el) {
+      el.observe('click', function() {
+         target = el.href.replace(/.*#/, '.');
+         el.up(target).hide();
+         if(hidden_input = el.previous("input[type=hidden]")) hidden_input.value = '1';
+      });
+  });
+  
+  $$('.add_nested_item').each(function(el) {
+    el.observe('click', function() {
+      template = eval(el.href.replace(/.*#/, ''));
+      $(el.rel).insert({     
+        bottom: replace_ids(template.gsub('&quot;', '\'')).unescapeHTML()
+      });
+    });
+  });
 });
