7f808c6a02f60b04a3e4dd5f41d1372b76f74bed
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-02-10 08:36:44 +0100

    Validations on Product numerical fields done

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@ class Product < ActiveRecord::Base
   belongs_to :tva
   
   delegate :value, :to => :tva, :prefix => true, :allow_nil => true
+  
+  validates_numericality_of :unit_price, :quantity
 end
