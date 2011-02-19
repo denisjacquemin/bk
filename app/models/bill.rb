478bf9a3281889c18a04d0fc93cedb6b4f05d543
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-02-19 16:45:31 +0100

    Money gem used in model Bill and Product

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -3,6 +3,22 @@ class Bill < ActiveRecord::Base
   has_many :products
   belongs_to :author, :class_name => "User", :foreign_key => "author_id"
   
+  composed_of :totaltvac,
+              :class_name => "Money",
+              :mapping => %w(totaltvac_in_cents cents),
+              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
+              :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
+
+  composed_of :totalhtva,
+              :class_name => "Money",
+              :mapping => %w(totalhtva_in_cents cents),
+              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
+              :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
+  
+  
+  validates_numericality_of :totaltvac, :totalhtva
+  validates :name, :reference, :presence => true
+  
   accepts_nested_attributes_for :products, :allow_destroy => true
   delegate :firstname, :to => :customer, :prefix => true, :allow_nil => true
   delegate :lastname, :to => :customer, :prefix => true, :allow_nil => true
@@ -10,8 +26,6 @@ class Bill < ActiveRecord::Base
   delegate :fullname_with_underscore, :to => :customer, :prefix => true, :allow_nil => true
   delegate :email, :to => :author, :prefix => true, :allow_nil => true
   delegate :fullname_with_reference, :to => :customer, :prefix => true, :allow_nil => true
-  
-  validates :name, :reference, :presence => true
   
   scope :by_company, lambda { |company_id| where("company_id = ?", company_id) }
   
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,25 @@ class Product < ActiveRecord::Base
   belongs_to :bill
   belongs_to :tva
   
-  delegate :value, :to => :tva, :prefix => true, :allow_nil => true
+  composed_of :totaltvac,
+              :class_name => "Money",
+              :mapping => %w(totaltvac_in_cents cents),
+              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
+              :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
+
+  composed_of :totalhtva,
+              :class_name => "Money",
+              :mapping => %w(totalhtva_in_cents cents),
+              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
+              :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
+              
+  composed_of :price,
+              :class_name => "Money",
+              :mapping => %w(price_in_cents cents),
+              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
+              :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
   
-  validates_numericality_of :unit_price, :quantity
+  validates_numericality_of :price, :totaltvac, :totalhtva, :quantity
+  
+  delegate :value, :to => :tva, :prefix => true, :allow_nil => true
 end
