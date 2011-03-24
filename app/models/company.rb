68f683ea54c765a75f854c1efcc098a1e760e8bd
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-03-24 08:30:16 +0100

    check if company informations are complete

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,7 +3,18 @@ class Company < ActiveRecord::Base
   has_many :customers
   belongs_to :address
   has_many :bankaccounts
+  has_many :tvas
   
   accepts_nested_attributes_for :address
   accepts_nested_attributes_for :bankaccounts, :allow_destroy => true
+  accepts_nested_attributes_for :tvas
+  
+  def completed?
+    complete = true
+    complete = false if name.empty? 
+    complete = false if address.nil? 
+    complete = false if address.city.empty?
+    complete = false if address.street.empty?
+    complete
+  end
 end
