27e476887a98e436dc3bc0b54e5cb9a26acc635b
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-04-04 07:25:40 +0200

    default Currency is EUR

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,19 +5,19 @@ class Product < ActiveRecord::Base
   composed_of :totaltvac,
               :class_name => "Money",
               :mapping => %w(totaltvac_in_cents cents),
-              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
+              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money::Currency.new(:eur)) },
               :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
 
   composed_of :totalhtva,
               :class_name => "Money",
               :mapping => %w(totalhtva_in_cents cents),
-              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
+              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money::Currency.new(:eur)) },
               :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
               
   composed_of :price,
               :class_name => "Money",
               :mapping => %w(price_in_cents cents),
-              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
+              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money::Currency.new(:eur)) },
               :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
   
   validates_numericality_of :price, :totaltvac, :totalhtva, :quantity
