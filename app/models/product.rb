21a0adb4469e09cebdac366089f8ade5868ecbb2
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-04-05 07:26:07 +0200

    remove numerical validation, needs to replace it with a validate_format because we use ',' as separator for decimal

--- app/models/product.rb
+++ app/models/product.rb
@@ -20,7 +20,7 @@ class Product < ActiveRecord::Base
               :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money::Currency.new(:eur)) },
               :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
   
-  validates_numericality_of :price, :totaltvac, :totalhtva, :quantity
+  validates_numericality_of :totaltvac, :totalhtva, :quantity
   
   delegate :value, :to => :tva, :prefix => true, :allow_nil => true
 end
