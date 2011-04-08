50618bccbaaa1bdd8ed9e31166ca7dddf7749758
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-04-08 07:10:03 +0200

    makes devise works again

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Billit::Application.config.secret_token = '46c3c7a89964b4701229d2f1178333e8a61f2d09879feea74b60ed4daf83bd9d748396534b56abdefc13b7a39e91c95fa626d6ec57c3055591b30f907a870e2e'
+Billit::Application.config.secret_token = '56c3c7a89964b4701229d2f1178333e8a61f2d09879feea74b60ed4daf83bd9d748396534b56abdefc13b7a39e91c95fa626d6ec57c3055591b30f907a870e2e'
