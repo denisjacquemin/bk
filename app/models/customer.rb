bc85c6e4c3a794ba391e272c66263bd2fd5bf02e
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-05-09 18:41:20 +0200

    conditional validation based on customer_type, and conditional fullname based on customer_type

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,15 +10,20 @@ class Customer < ActiveRecord::Base
   
   delegate :city, :to => :address, :prefix => true, :allow_nil => true
   
-  validates :firstname, :lastname, :reference, :presence => true
-  validates_uniqueness_of :reference
+  validates_uniqueness_of :reference, :presence => true
+  
+  validate :presence_of_firstname_and_lastname_or_company_name
   
   scope :by_company, lambda { |company_id| where("company_id = ?", company_id) }
   scope :active, where('deleted = ?', false)
   scope :latest, order('updated_at desc')
   
   def fullname
-    "#{self.firstname} #{self.lastname}"
+    if customer_type == 1
+      "#{self.firstname} #{self.lastname}"
+    elsif customer_type == 2
+      company_name
+    end
   end
   
   def fullname_with_underscore
@@ -27,5 +32,13 @@ class Customer < ActiveRecord::Base
   
   def fullname_with_reference
     "#{self.fullname} (ref: #{self.reference})"
+  end
+  
+  def presence_of_firstname_and_lastname_or_company_name
+    if customer_type == 1
+      validates_presence_of :firstname, :lastname
+    elsif customer_type == 2
+      validates_presence_of :company_name
+    end
   end
 end
